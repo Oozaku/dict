86b2cc19fb1d407355d10a8d35615e34bb159096
Willian Ozako <willian.ozako@gmail.com>
2022-06-30 18:51:58 -0300

    Fix owlbot logic

--- getdef/owlbot/owlbot.go
+++ getdef/owlbot/owlbot.go
@@ -15,32 +15,34 @@ import (
 
 // Struct Owlbot holds the token used to authenticate requests
 type Owlbot struct {
-	Token string
+	Token string `json:"token"`
 }
 
 // Owlbot's endpoint
 const ENDPOINT string = "https://owlbot.info/api/v4/dictionary/"
 
 // Method to search for definitions in Owlbot API
-func (owl Owlbot) SearchDefinition(words []string) []word.Word {
+func (owl Owlbot) SearchDefinition(words []string) ([]word.Word, error) {
 
 	// Build query and head for request
 	query := strings.Join(words, "+")
 	url := ENDPOINT + query
 	headers := make(map[string]string)
-	headers["Authorization"] = fmt.Sprint("Token", owl.Token)
+	headers["Authorization"] = fmt.Sprint("Token ", owl.Token)
 
 	// Make GET request
 	raw, err := requests.Get(url, headers)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return []word.Word{}, err
 	}
 
 	// Unmarshal result into string
 	owlbot := Word{}
 	err = json.Unmarshal(raw, &owlbot)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return []word.Word{}, err
 	}
 
 	// Converted results will be stored hear
@@ -61,5 +63,5 @@ func (owl Owlbot) SearchDefinition(words []string) []word.Word {
 	}
 
 	// Return result
-	return []word.Word{result}
+	return []word.Word{result}, nil
 }
